f84fc0636e2c3f358387fe7316e7174a5ffe716a
lwc414 <lwc414@gmail.com>
2022-11-12 17:28:14 +0800

    latest version172800

--- BudgetService.java
+++ BudgetService.java
@@ -6,72 +6,72 @@ import java.util.HashMap;
 
 public class BudgetService
 {
-	private final DateTimeFormatter dffull = DateTimeFormatter.ofPattern("yyyyMMdd");
-	private final DateTimeFormatter dfmonth = DateTimeFormatter.ofPattern("yyyyMM");
+    private final DateTimeFormatter dffull = DateTimeFormatter.ofPattern("yyyyMMdd");
+    private final DateTimeFormatter dfmonth = DateTimeFormatter.ofPattern("yyyyMM");
 
-	private IBudgetRepo implBudget;
+    private IBudgetRepo implBudget;
 
-	public BudgetService(IBudgetRepo impl)
-	{
-		this.implBudget = impl;
-	}
+    public BudgetService(IBudgetRepo impl)
+    {
+        this.implBudget = impl;
+    }
 
-	public Double query(String strStartDate, String strEndDate)
-	{
-		double dblTotalBudget = 0.0;
-		LocalDate ldStart = this.validateInputDate(strStartDate), ldEnd = this.validateInputDate(strEndDate);
+    public Double query(String strStartDate, String strEndDate)
+    {
+        double dblTotalBudget = 0.0;
+        LocalDate ldStart = this.validateInputDate(strStartDate), ldEnd = this.validateInputDate(strEndDate);
 
-		if (null == ldStart || null == ldEnd || (!ldEnd.equals(ldStart) && ldEnd.isBefore(ldStart)))
-			return dblTotalBudget;
+        if (null == ldStart || null == ldEnd || (!ldEnd.equals(ldStart) && ldEnd.isBefore(ldStart)))
+            return dblTotalBudget;
 
-		LocalDate ldOperationDate = ldStart;
-		List<Budget> lstAllBudget = this.implBudget.getAll();
-		HashMap<String, Budget> hmpAllBudget = new HashMap<String, Budget>(lstAllBudget.size(), 1f);
+        LocalDate ldOperationDate = ldStart;
+        List<Budget> lstAllBudget = this.implBudget.getAll();
+        HashMap<String, Budget> hmpAllBudget = new HashMap<String, Budget>(lstAllBudget.size(), 1f);
 
-		for (Budget b : lstAllBudget)
-			hmpAllBudget.put(b.strBudgetPeriod, b);
+        for (Budget b : lstAllBudget)
+            hmpAllBudget.put(b.strBudgetPeriod, b);
 
-System.out.println(ldStart + ";" + ldEnd);
+        System.out.println(ldStart + ";" + ldEnd);
 
-		while (ldOperationDate.isBefore(ldEnd) || ldEnd.equals(ldStart))
-		{
-			int intDayCountOfMonth = ldOperationDate.getMonth().length(ldOperationDate.isLeapYear());
-			LocalDate endDateOfMonth = ldOperationDate.withDayOfMonth(intDayCountOfMonth);
-			String strOperatingPeriod = ldOperationDate.format(dfmonth); //YearMonth
+        while (ldOperationDate.isBefore(ldEnd) || ldEnd.equals(ldStart))
+        {
+            int intDayCountOfMonth = ldOperationDate.getMonth().length(ldOperationDate.isLeapYear());
+            LocalDate endDateOfMonth = ldOperationDate.withDayOfMonth(intDayCountOfMonth);
+            String strOperatingPeriod = ldOperationDate.format(dfmonth); //YearMonth
 
-System.out.println("loop -> " + strOperatingPeriod);
+            System.out.println("loop -> " + strOperatingPeriod);
 
-			if (hmpAllBudget.containsKey(strOperatingPeriod)) // ·í¤ë¦³¹wºâ
-			{
-				double dblPeriodBudget = hmpAllBudget.get(strOperatingPeriod).dblBudget;
-				intDayCountOfMonth = ldOperationDate.getMonth().length(ldOperationDate.isLeapYear());
-				endDateOfMonth = ldOperationDate.withDayOfMonth(intDayCountOfMonth);
-				long lngComputingDayCOunt = ChronoUnit.DAYS.between(ldOperationDate, (ldEnd.isBefore(endDateOfMonth) ? ldEnd : endDateOfMonth)) + 1;
+            if (hmpAllBudget.containsKey(strOperatingPeriod)) // ç•¶æœˆæœ‰é ç®—
+            {
+                double dblPeriodBudget = hmpAllBudget.get(strOperatingPeriod).dblBudget;
+                intDayCountOfMonth = ldOperationDate.getMonth().length(ldOperationDate.isLeapYear());
+                endDateOfMonth = ldOperationDate.withDayOfMonth(intDayCountOfMonth);
+                long lngComputingDayCOunt = ChronoUnit.DAYS.between(ldOperationDate, (ldEnd.isBefore(endDateOfMonth) ? ldEnd : endDateOfMonth)) + 1;
 
-System.out.println("loop with budget-> " + dblPeriodBudget + "; daycount=" + intDayCountOfMonth + ";enddate=" + endDateOfMonth + ";lowerbound=" + ldOperationDate);
-System.out.println("computing day count = >> " + lngComputingDayCOunt);
+                System.out.println("loop with budget-> " + dblPeriodBudget + "; daycount=" + intDayCountOfMonth + ";enddate=" + endDateOfMonth + ";lowerbound=" + ldOperationDate);
+                System.out.println("computing day count = >> " + lngComputingDayCOunt);
 
-				dblTotalBudget += dblPeriodBudget * (lngComputingDayCOunt / (double) intDayCountOfMonth);
-			}
+                dblTotalBudget += dblPeriodBudget * (lngComputingDayCOunt / (double) intDayCountOfMonth);
+            }
 
-			if (ldEnd.isAfter(endDateOfMonth))
-				ldOperationDate = ldOperationDate.withDayOfMonth(1);
-			else if (ldEnd.equals(ldOperationDate))
-				break;
+            if (ldEnd.isAfter(endDateOfMonth))
+                ldOperationDate = ldOperationDate.withDayOfMonth(1);
+            else if (ldEnd.equals(ldOperationDate))
+                break;
 
-			ldOperationDate = ldOperationDate.plusMonths(1);
+            ldOperationDate = ldOperationDate.plusMonths(1);
 
-		}
-		return dblTotalBudget;
-	}
+        }
+        return dblTotalBudget;
+    }
 
-	private LocalDate validateInputDate(String strDate)
-	{
-		try { return LocalDate.parse(strDate, this.dffull); }
-		catch (Exception exc)
-		{
-			System.out.println("Invalid Input: " + strDate);
-			return null;
-		}
-	}
+    private LocalDate validateInputDate(String strDate)
+    {
+        try { return LocalDate.parse(strDate, this.dffull); }
+        catch (Exception exc)
+        {
+            System.out.println("Invalid Input: " + strDate);
+            return null;
+        }
+    }
 }
--- BudgetTest.java
+++ BudgetTest.java
@@ -1,20 +1,60 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 
 public class BudgetTest
 {
-	public static void main(String[] straryParameters)
-	{
-//		System.out.println(ChronoUnit.DAYS.between(
-//				LocalDate.parse("20221001", DateTimeFormatter.ofPattern("yyyyMMdd")),
-//				LocalDate.parse("20220930", DateTimeFormatter.ofPattern("yyyyMMdd"))));
-//
-//		System.out.println(LocalDate.parse("20221005", DateTimeFormatter.ofPattern("yyyyMMdd")).format(DateTimeFormatter.ofPattern("yyyyMM")));
-//		System.out.println(ld.withDayOfMonth(ld.getMonth().length(ld.isLeapYear())));
-		
-		BudgetService bs = new BudgetService(new BudgetRepository());
-		System.out.println(bs.query("20220101", "20220102"));
+	BudgetService bs = new BudgetService(new BudgetRepository());
+
+	@Test
+	public void Oneday() {
+		Double Total =bs.query("20220301","20220301");
+		assertEquals(10.0,Total);
+
+	}
+
+	@Test
+	public void HoldMonth() {
+		Double Total =bs.query("20220301","20220331");
+		assertEquals(310.0,Total);
+	}
+
+
+	@Test
+	public void CrossMonth() {
+		Double Total =bs.query("20220421","20220531");
+		assertEquals(410.0,Total);
+	}
+
+	@Test
+	public void CrossMonth2() {
+		Double Total =bs.query("20220315","20220815");
+		assertEquals(930.0,Total);
+	}
+
+
+	@Test
+	public void CrossMonth3() {
+		Double Total =bs.query("20210315","20230101");
+		assertEquals(2760.0,Total);
+	}
+
+	@Test
+	public void Error_Time() {
+		Double Total =bs.query("20220301","20220201");
+		assertEquals(0.0,Total);
+	}
+
+	@Test
+	public void Empty() {
+		Double Total =bs.query("","");
+		assertEquals(0.0,Total);
 	}
 }
